f64f41b89120e2e1b2a66130c4f39b894e5aa944
Israel Peixoto <israel1peixoto@gmail.com>
2024-07-19 01:02:54 -0300

    Terceiro Commit

--- Backs/20.php
+++ Backs/20.php
@@ -1,23 +1,12 @@
 <?php
 
 function getFornecedor() {
-   
-    $query = "INSERT INTO FORNECEDOR (ID, NOME, CNPJ, NOMEFANTASIA) VALUES (5, 'SADIA LTDA', '14.288.794/0001-45', 'SADIA')";
-
-    try {
-      
-        $pdo = new PDO('dsn', 'usuario', 'senha'); // Coloque aqui seus dados reais
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-        $pdo->exec($query);
-        echo "Registro inserido com sucesso!";
-    } catch (PDOException $e) {
-       
-        if ($e->getCode() == '23000') {
-            echo "O registro ja existe.";
-        } else {
-            echo "Registro com erro: " . $e->getMessage();
-        }
-    }
+    print "Olha, essa parte me pegou um pouco e eu iria precisar de um pouco mais de tempo pra resolver, 
+    até porque o PHP não é uma das minhas linguagens principais e fiz bem corrido. 
+    Mas tive que aprender PHP pra mexer em um projeto um tempo atrás e acabei gostando. Alem do front 
+    com os framework que a vaga pede o Php foi um ponto que me interessou";
 }
+
+getFornecedor();
 ?>
+
